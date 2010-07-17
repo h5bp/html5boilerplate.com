e2c547f4d965b573f162c2cac11f2281b98e4799
paulirish <paul.irish@gmail.com>
2010-07-17 13:47:22 -0700

    update the boilerplate template

--- template/js/plugins.js
+++ template/js/plugins.js
@@ -1,5 +1,4 @@
 
-
 // remap jQuery to $
 (function($){
 
@@ -13,4 +12,32 @@
 
 
 
-})(jQuery);
+})(window.jQuery);
+
+
+
+// usage: log('inside coolFunc',this,arguments);
+// http://paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+window.log = function(){
+  log.history = log.history || [];   // store logs to an array for reference
+  log.history.push(arguments);
+  if(this.console){
+    console.log( Array.prototype.slice.call(arguments) );
+  }
+};
+
+
+
+// catch all document.write() calls
+(function(){
+  var docwrite = document.write;
+  document.write = function(q){ 
+    log('document.write(): ',q); 
+    if (/docwriteregexwhitelist/.test(q)) docwrite(q);  
+  }
+})();
+
+
+// background image cache bug for ie6.  via: http://www.mister-pixel.com/#Content__state=
+/*@cc_on   @if (@_win32) { document.execCommand("BackgroundImageCache",false,true) }   @end @*/
+
