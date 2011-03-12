7545a67b05c09e0d00a387cc44a0e8508e10098c
Shi Chuan <shichuanr@gmail.com>
2011-03-13 01:53:49 +0800

    added blocktrans to template files

--- src/template/js/plugins.js
+++ src/template/js/plugins.js
@@ -1,15 +1,15 @@
 
-// usage: log('inside coolFunc', this, arguments);
+// {% blocktrans %}usage: log('inside coolFunc', this, arguments);{% endblocktrans %}
 // paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
 window.log = function(){
-  log.history = log.history || [];   // store logs to an array for reference
+  log.history = log.history || [];   // {% blocktrans %}store logs to an array for reference{% endblocktrans %}
   log.history.push(arguments);
   arguments.callee = arguments.callee.caller;  
   if(this.console) console.log( Array.prototype.slice.call(arguments) );
 };
-// make it safe to use console.log always
+// {% blocktrans %}make it safe to use console.log always{% endblocktrans %}
 (function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info, log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
 
 
-// place any jQuery/helper plugins in here, instead of separate, slower script files.
+// {% blocktrans %}place any jQuery/helper plugins in here, instead of separate, slower script files.{% endblocktrans %}
 
