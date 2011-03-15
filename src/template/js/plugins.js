72b64c5268dab6d7de6fdad35b12b42a8cf8893b
Divya Manian <divya.manian@gmail.com>
2011-03-15 15:24:03 -0700

    Updating to changes shichuan did for templates

--- src/template/js/plugins.js
+++ src/template/js/plugins.js
@@ -1,40 +1,15 @@
 
-// {% blocktrans %}remap jQuery to ${% endblocktrans %}
-(function($){
-
- 
-
-
-
-
-
- 
-
-
-
-})(this.jQuery);
-
-
-
-// {% blocktrans %}usage{% endblocktrans %}: log('inside coolFunc',this,arguments);
+// {% blocktrans %}usage: log('inside coolFunc', this, arguments);{% endblocktrans %}
 // paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
 window.log = function(){
   log.history = log.history || [];   // {% blocktrans %}store logs to an array for reference{% endblocktrans %}
   log.history.push(arguments);
-  if(this.console){
-    console.log( Array.prototype.slice.call(arguments) );
-  }
+  arguments.callee = arguments.callee.caller;  
+  if(this.console) console.log( Array.prototype.slice.call(arguments) );
 };
+// {% blocktrans %}make it safe to use console.log always{% endblocktrans %}
+(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info, log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
 
 
-
-// {% blocktrans %}catch all document.write() calls{% endblocktrans %}
-(function(doc){
-  var write = doc.write;
-  doc.write = function(q){ 
-    log('document.write(): ',arguments); 
-    if (/docwriteregexwhitelist/.test(q)) write.apply(doc,arguments);  
-  };
-})(document);
-
+// {% blocktrans %}place any jQuery/helper plugins in here, instead of separate, slower script files.{% endblocktrans %}
 
