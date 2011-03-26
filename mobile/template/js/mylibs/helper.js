188a0715da165cf6a613c2110c414361b8ca8676
Paul Irish <paul.irish@gmail.com>
2011-03-26 14:32:15 -0700

    whitespace adjustment.

--- mobile/template/js/mylibs/helper.js
+++ mobile/template/js/mylibs/helper.js
@@ -96,25 +96,25 @@ MBP.splash = function () {
 // http://googlecode.blogspot.com/2009/07/gmail-for-mobile-html5-series.html
 
 MBP.autogrow = function (element, lh) {
-    
-    function handler(e){
-        var newHeight = this.scrollHeight,
-            currentHeight = this.clientHeight;
-        if (newHeight > currentHeight) {
-            this.style.height = newHeight + 3 * textLineHeight + "px";
-        }
+  
+  function handler(e){
+    var newHeight = this.scrollHeight,
+      currentHeight = this.clientHeight;
+    if (newHeight > currentHeight) {
+      this.style.height = newHeight + 3 * textLineHeight + "px";
     }
-    
-    var setLineHeight = (lh) ? lh : 12,
-        textLineHeight = element.currentStyle ? element.currentStyle.lineHeight : 
-                         getComputedStyle(element, null).lineHeight;
-                         
-    textLineHeight = (textLineHeight.indexOf("px") == -1) ? setLineHeight :
-                     parseInt(textLineHeight, 10);
+  }
+  
+  var setLineHeight = (lh) ? lh : 12,
+    textLineHeight = element.currentStyle ? element.currentStyle.lineHeight : 
+             getComputedStyle(element, null).lineHeight;
+             
+  textLineHeight = (textLineHeight.indexOf("px") == -1) ? setLineHeight :
+           parseInt(textLineHeight, 10);
 
-    element.style.overflow = "hidden";
-    element.addEventListener ? element.addEventListener('keyup', handler, false) :
-                               element.attachEvent('onkeyup', handler);
+  element.style.overflow = "hidden";
+  element.addEventListener ? element.addEventListener('keyup', handler, false) :
+                 element.attachEvent('onkeyup', handler);
 }
 
 
