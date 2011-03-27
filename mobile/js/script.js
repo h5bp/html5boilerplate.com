a938faf09b67506242da57aead70b0c06ac8d81e
Shi Chuan <shichuanr@gmail.com>
2011-03-27 20:42:22 +0800

    updated content to mobile bp

--- mobile/js/script.js
+++ mobile/js/script.js
@@ -11,47 +11,55 @@ var local = [
  
  
 function mycallback(data,status,xhr,filename){
-  //console.log(arguments);
  
   data = data.replace(/</g,'&lt;');
- 
  
   var fileext = filename.replace('htaccess.txt?.xml','htaccess.apache').split('.').slice(-1)[0];
   var elem = $('<textarea>').addClass('brush:'+fileext).html(data).css('visibility','hidden');
   var point = $('#thesource').nextAll('textarea, .syntaxhighlighter').andSelf().last();
  
   elem.insertAfter(point);
-  $('<h4>').text(filename.replace('?.xml','').replace('htaccess.txt','.htaccess')).insertAfter(point);
+  $('<h4>').html('<a>'+filename.replace('?.xml','').replace('htaccess.txt','.htaccess')+'</a>').insertAfter(point);
  
   var x = SyntaxHighlighter.highlight(undefined, elem[0]);
- 
  
   if (mycallback.countdown === undefined) mycallback.countdown = local.length;
  
   mycallback.countdown--;
  
   if (mycallback.countdown === 0){ 
+    // not sure why the movile viewport block doesnt get commented but.. it doesnt
+    var viewport = $('code.plain:contains(Mobile Viewport Fix)').closest('div.line');
+    viewport.nextAll().slice(0,5).add(viewport).find('.plain').toggleClass('plain comments');
  
-// not sure why the movile viewport block doesnt get commented but.. it doesnt
-var viewport = $('code.plain:contains(Mobile Viewport Fix)').closest('div.line');
-viewport.nextAll().slice(0,5).add(viewport).find('.plain').toggleClass('plain comments');
- 
-$('code.comments').each(function(){
-  $(this).html(  linkify( this.innerHTML ) );
-})
+    $('code.comments').each(function(){
+      $(this).html(  linkify( this.innerHTML ) );
+    })
+    $('#source h4').next().hide('slow');
+    $('#source h4').toggle(
+      function(e) {
+        $(this).addClass('active');
+        $(this).next().show('slow');
+        e.preventDefault();
+      },
+      function(e) {
+        $(this).removeClass('active');
+        $(this).next().hide('slow');
+        e.preventDefault();
+      }        
+    );
   }
- 
- 
- 
+  
 }
 
-$('#thesource').hide();
+
 if(!jQuery.browser.mobile) {
-	$('#thesource').show();
+  $('#source').show();
   $('#frame').html('<img src="images/devices.png" /><div><img src="images/logos.png" /></div>');
+  $('#ppt').html('<iframe src="https://docs.google.com/present/embed?id=dkx3qtm_20f3cvs8gc" frameborder="0" width="410" height="342"></iframe> <iframe src="https://docs.google.com/present/embed?id=dkx3qtm_22dxsrgcf4" frameborder="0" width="410" height="342"></iframe>');
 }
 
-$('#thesource').click(function() {
+//$('#thesource').click(function() {
   $(this).html('Walk Through It');
 	$.each(local,function(k,path){
 		$.ajax({
@@ -65,9 +73,11 @@ $('#thesource').click(function() {
 		url : path
 		})
 	});
-	return false;
-});
- 
+
+  
+	//return false;
+//});
+  
 SyntaxHighlighter.config.tagName = 'textarea';
 SyntaxHighlighter.defaults['wrap-lines'] = true; // maybe change this
 SyntaxHighlighter.defaults['auto-links'] = false;
