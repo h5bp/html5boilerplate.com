9877e146d206a76fd7c5711dde7e2466564a52a6
Divya Manian <divya.manian@gmail.com>
2011-08-07 18:31:08 -0700

    updated the templates to match latest files

--- src/template/js/plugins.js
+++ src/template/js/plugins.js
@@ -4,12 +4,15 @@
 window.log = function(){
   log.history = log.history || [];   // {% blocktrans %}store logs to an array for reference{% endblocktrans %}
   log.history.push(arguments);
-  arguments.callee = arguments.callee.caller;  
-  if(this.console) console.log( Array.prototype.slice.call(arguments) );
+  if(this.console) {
+   arguments.callee = arguments.callee.caller;
+   var newarr = [].slice.call(arguments);
+   (typeof console.log === 'object' ? log.apply.call(console.log, console, newarr) : console.log.apply(console, newarr));
+  }
 };
 // {% blocktrans %}make it safe to use console.log always{% endblocktrans %}
-(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info, log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
-
+(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,timeStamp,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();){b[a]=b[a]||c}})((function(){try
+{console.log();return window.console;}catch(err){return window.console={};}})());
 
 // {% blocktrans %}place any jQuery/helper plugins in here, instead of separate, slower script files.{% endblocktrans %}
 
