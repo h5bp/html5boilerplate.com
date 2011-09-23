68140b9235ee548684231142b0ca32c173fb0d70
Shi Chuan <shichuanr@gmail.com>
2011-09-23 22:37:12 +0800

    added mobile path

--- mobile/js/script.js
+++ mobile/js/script.js
@@ -27,11 +27,11 @@ MBP.gestureStart = function () {
 }
 
 var local = [
- './template/index.html',
- './template/css/style.css',
- './template/js/mylibs/helper.js',
- './template/sitemap.xml',
- './template/htaccess.txt?.xml'
+ './mobile/template/index.html',
+ './mobile/template/css/style.css',
+ './mobile/template/js/mylibs/helper.js',
+ './mobile/template/sitemap.xml',
+ './mobile/template/htaccess.txt?.xml'
 ];
  
  
